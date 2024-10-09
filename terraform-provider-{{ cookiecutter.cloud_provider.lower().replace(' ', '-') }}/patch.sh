cd556c59bd27bc4cc9747a8121216d4d514c87a8
squat <lserven@gmail.com>
2024-10-09 15:35:21 +0200

    nix: use flakes in generated repo
    Signed-off-by: squat <lserven@gmail.com>

--- terraform-provider-{{ cookiecutter.cloud_provider.lower().replace(' ', '-') }}/patch.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env sh
-# Setup
-F=$(mktemp)
-trap 'cat "$F"; exit' EXIT
-export YQ="go run github.com/mikefarah/yq/v4@v4.35.2"
-cat - > "$F"
-# General Patches
-$YQ -i '.info.title = "{{ cookiecutter.cloud_provider }} Provider"' "$F"
